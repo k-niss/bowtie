b615f295786bc735973fe2f5e2192ebc43232735
kniss <kristofferniss@cpr.ku.dk>
2020-03-05 13:55:24 +0100

    adding data to R package

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' cDC1 adjacency matrix
+#'
+#' An adjacency matrix of the proteins in the cDC1 cell containing the confidence
+#' scores of each pairiwse protein-protein interaction (PPI). The confidence scores
+#' have been obtained from PPI interactome InWeb_IM.
+#'
+#' @format An 8568 x 8568 data matrix containing pairwise confidence score for each protein pair
+#'
+"cDC1_adj_matrix"
