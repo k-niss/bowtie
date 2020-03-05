e34811fd82404cbd7ddf5d680bb20fb1e1bb473c
k-niss <46531621+k-niss@users.noreply.github.com>
2020-03-05 14:09:26 +0100

    Update data.R

--- R/data.R
+++ R/data.R
@@ -4,6 +4,14 @@
 #' scores of each pairiwse protein-protein interaction (PPI). The confidence scores
 #' have been obtained from PPI interactome InWeb_IM.
 #'
+#' @docType data
+#'
+#' @usage data(cDC1_adj_matrix)
+#'
 #' @format An 8568 x 8568 data matrix containing pairwise confidence score for each protein pair
 #'
+#' @keywords adjacency
+#'
+#' @examples
+#' data(cDC1_adj_matrix)
 "cDC1_adj_matrix"
